5117e7d6972627faace98f57881660cc16f7b6ed
radya100 <radya100@gmail.com>
2023-11-04 11:26:00 +0300

    feat: добавил БД и скрипты инициализации

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,52 @@
+create table if not exists _user(
+    id int generated always as identity primary key
+    , email VARCHAR(256)
+    , login varchar(256)
+    , name varchar(512)
+    , birthday DATE
+);
+create table if not exists rating
+(
+    id int generated always as identity primary key
+    , name varchar(256)
+);
+create table if not exists film
+(
+    id int generated always as identity primary key
+    , name varchar(256)
+    , description varchar(200)
+    , release_date DATE
+    , duration int
+    , rating_id int
+    , foreign key (rating_id) references rating(id)
+);
+create table if not exists user_friend
+(
+    user_id int
+    , related_user_id int
+    , primary key (user_id, related_user_id)
+    , foreign key (user_id) references _user(id)
+    , foreign key (related_user_id) references  _user(id)
+);
+create table if not exists user_like_film
+(
+    user_id int
+    , film_id int
+    , primary key (user_id, film_id)
+    , foreign key (user_id) references _user(id)
+    , foreign key (film_id) references film(id)
+);
+create table if not exists genre
+(
+    id int generated always as identity primary key
+    , name varchar(256)
+);
+create table if not exists film_genre
+(
+    film_id int
+    , genre_id int
+    , primary key (film_id, genre_id)
+    , foreign key (film_id) references film(id)
+    , foreign key (genre_id) references genre(id)
+);
+
